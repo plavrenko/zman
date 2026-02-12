d4ca7c42900831fb7874ecb334e4e9d9d4bfff59
Pavel Lavrenko <pavel@lavrenko.info>
2026-02-12 19:51:05 +0200

    Initial Commit

---
+++ Zman-claude/Zman_claudeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zman_claudeApp.swift
+//  Zman-claude
+//
+//  Created by Pavel Lavrenko on 12/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct Zman_claudeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
